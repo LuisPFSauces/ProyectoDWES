0ce2ef4471dd59046f7eabc9a775e57f2e76719d
Luis Puente Fernandez <luis.puefer@educa.jcyl.es>
2020-10-24 23:30:49 +0200

    Primer Commit, añadidos todos los ficheros creados hasta la fecha de creación

---
+++ indexProyectoDWES.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="webroot/css/estilo.css">
+        <title>Document</title>
+    </head>
+
+    <body>
+        <a href="doc/20200925EjercicioTema1_LuisPuente.pdf" class="tarjeta tarjeta1">
+            <h2>Tema 1</h2>
+        </a>
+        <a href="doc/2020-09-29-Manual.pdf" class="tarjeta tarjeta2">
+            <h2>Tema 2</h2>
+        </a>
+        <a href="proyectoTema3/indexProyectoTema3.php" class="tarjeta tarjeta1">
+            <h2>Tema 3</h2>
+        </a>
+        <a href="webroot/css/estilo.css" class="tarjeta tarjeta1">
+            <h2>Tema 1</h2>
+        </a>
+        <a href="webroot/css/estilo.css" class="tarjeta tarjeta1">
+            <h2>Tema 1</h2>
+        </a>
+         <a href="../index.html" class="tarjeta tarjeta2">
+            <h2>Atras</h2>
+        </a>
+    </body>
+
+</html>
